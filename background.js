8bcc7903ffc88df7a9c9686e55ae42091f922134
Aaron Lin <lin.aaron1@gmail.com>
2015-10-24 10:01:08 -0700

    alert highlighted text now

--- background.js
+++ background.js
@@ -1,20 +1,14 @@
-/*chrome.tabs.executeScript( {
-  code: "window.getSelection().toString();"
-}, function(selection) {
-  document.innerHTML = selection[0];
-});*/
-
 
 chrome.runtime.onInstalled.addListener(function() {
 	chrome.contextMenus.create({
 		id: "searchword",
-		title: "@@ Search Word with X",
+		title: "@@[%s] with X",
 		contexts: ["selection"]
 	});
 });
 
 chrome.contextMenus.onClicked.addListener(
 	function(info, tab) {
-		alert("hello world!");
+		alert(info.selectionText);
 	}
 );
